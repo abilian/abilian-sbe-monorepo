55a109d6e67693c23ce61775df6d36257307d7cd
Stefane Fermigier <sf@abilian.com>
2021-09-20 16:19:27 +0200

    devops: remove debug code.

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,6 @@
 """Create an application instance."""
 
-import os
 import sys
-import rich
-
-env = {}
-for k in sorted(os.environ):
-    env[k] = os.environ[k]
-rich.print(env)
-
-rich.print("path=", sys.path)
 
 sys.path = ["src"] + sys.path
 
