eb4cd9135c1c5122ee79d398b53d96d32387eb7d
Stefane Fermigier <sf@abilian.com>
2022-03-28 17:28:55 +0200

    fix: run all tests.

--- src/abilian/core/extensions/login.py
+++ src/abilian/core/extensions/login.py
@@ -4,15 +4,16 @@ from typing import TYPE_CHECKING
 
 from flask_login import AnonymousUserMixin, LoginManager
 
+
 if TYPE_CHECKING:
     from abilian.core.models.subjects import Group
 
 
 class AnonymousUser(AnonymousUserMixin):
     def has_role(self, role):
-        from abilian.services import get_service
+        from abilian.services import get_security_service
 
-        security = get_service("security")
+        security = get_security_service()
         return security.has_role(self, role)
 
     @property
