6e9a25cc5a646abab302c3088e92b1fd65e0dd27
Stefane Fermigier <sf@abilian.com>
2022-04-04 16:58:44 +0200

    refact: add flask-tailwind files.

---
+++ tailwind/postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [
+    require("postcss-import"),
+    require("postcss-each"),
+    require("postcss-nested"),
+    require("tailwindcss"),
+  ],
+};
---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,86 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  // Make Tailwind and Bootstrap 4 cohabitate for now.
+  corePlugins: { preflight: false },
+  prefix: "tw-",
+
+  content: [
+    /**
+     * HTML. Paths to Flask template files that may contain Tailwind CSS classes.
+     */
+    "../src/**/templates/**/*.html",
+    "../src/**/templates/**/*.j2",
+
+    /**
+     * JS: If you use Tailwind CSS in JavaScript, uncomment the following lines and make sure
+     * patterns match your project structure.
+     */
+    /* JS 1: Ignore any JavaScript in node_modules folder. */
+    // '!../../**/node_modules',
+    /* JS 2: Process all JavaScript files in the project. */
+    // '../../**/*.js',
+
+    /**
+     * Python: If you use Tailwind CSS classes in Python, uncomment the following line
+     * and make sure the pattern below matches your project structure.
+     */
+    "../src/**/*.py",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    /**
+     * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
+     * for forms. If you don't like it or have own styling for forms,
+     * comment the line below to disable '@tailwindcss/forms'.
+     */
+    require('daisyui'),
+    // require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+
+  daisyui: {
+    // prefix: "dui-",
+    themes: [
+      {
+        'abilian': {                          /* your theme name */
+          'primary': colors.sky[400],           /* Primary color */
+          'primary-focus': colors.sky[600],     /* Primary color - focused */
+          'primary-content': '#ffffff',   /* Foreground content color to use on primary color */
+
+          'secondary': colors.amber[400],         /* Secondary color */
+          'secondary-focus': colors.amber[600],   /* Secondary color - focused */
+          'secondary-content': '#ffffff', /* Foreground content color to use on secondary color */
+
+          'accent': colors.teal[500],            /* Accent color */
+          'accent-focus': colors.teal[700],      /* Accent color - focused */
+          'accent-content': '#ffffff',    /* Foreground content color to use on accent color */
+
+          'neutral': colors.zinc[500],           /* Neutral color */
+          'neutral-focus': colors.zinc[700],     /* Neutral color - focused */
+          'neutral-content': '#ffffff',   /* Foreground content color to use on neutral color */
+
+          'base-100': '#ffffff',          /* Base color of page, used for blank backgrounds */
+          'base-200': '#f9fafb',          /* Base color, a little darker */
+          'base-300': '#d1d5db',          /* Base color, even more darker */
+          'base-content': '#1f2937',      /* Foreground content color to use on base color */
+
+          // 'info': '#2094f3',              /* Info */
+          // 'success': '#009485',           /* Success */
+          // 'warning': '#ff9900',           /* Warning */
+          // 'error': '#ff5724',             /* Error */
+
+          'info': colors.blue[600],
+          'success': colors.green[600],
+          'warning': colors.orange[600],
+          'error': colors.red[600],
+        },
+      },
+    ],
+  },
+
+};
