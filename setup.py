96665601055b76d9327662af1de0ec60500f280d
Jerome Dumonteil <jd@abilian.com>
2023-10-20 14:47:11 +0200

    fix: bump version  and upgrade setup.py for slapos

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
+import re # toml not available at install time on slapos
+
+from pathlib import Path
 from setuptools import setup
 
+
+_pyproj = Path("./pyproject.toml").read_text(encoding="utf8")
 setup(
     name="abilian-sbe",
-    version="1.0.18",
+    version=re.search(r'version *= *"(.*)"' , _pyproj).group(1),
     package_dir={"": "src"},
     packages=["abilian", "extranet"],
 )
