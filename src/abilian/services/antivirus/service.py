e3a4754f87bf8cc7f41e61a68af154d9c2675c3b
Jerome Dumonteil <jd@abilian.com>
2023-10-27 11:48:18 +0200

    feat: use ENV variable CLAMD_CONF_PATH

--- src/abilian/services/antivirus/service.py
+++ src/abilian/services/antivirus/service.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 import io
 import logging
 import os
-import pathlib
+from pathlib import Path
 
 from abilian.core.models.blob import Blob
 
@@ -13,48 +13,59 @@ from ..base import Service
 logger = logging.getLogger(__name__)
 
 try:
-    from clamd import ClamdUnixSocket, ClamdError
+    from clamd import ClamdError, ClamdUnixSocket
 
-    clamd = ClamdUnixSocket()
+    found_clamd = True
 except ImportError:
-    clamd = None
+    found_clamd = False
 
-CLAMD_CONF = {"StreamMaxLength": "25M", "MaxFileSize": "25M"}
-CLAMD_STREAMMAXLENGTH = 26214400
-CLAMD_MAXFILESIZE = 26214400
+CLAMD_CONF = {}
+CLAMD_CONF_DEFAULT = {
+    "StreamMaxLength": 26214400,
+    "MaxFileSize": 26214400,
+    "LocalSocket": "/run/clamav/clamd.sock",
+}
 
-if clamd:
-    conf_path = pathlib.Path("/etc", "clamav", "clamd.conf")
-    if conf_path.exists():
-        conf_lines = [line.strip() for line in conf_path.open("rt").readlines()]
-        CLAMD_CONF = dict(
-            line.split(" ", 1) for line in conf_lines if not line.startswith("#")
-        )
 
-        def _size_to_int(size_str):
-            multiplier = 0
-            if not size_str:
-                return 0
+def _size_to_int(size_str):
+    multiplier = 0
+    if not size_str:
+        return 0
 
-            unit = size_str[-1].lower()
-            if unit in ("k", "m"):
-                size_str = size_str[:-1]
-                multiplier = 1024
-                if unit == "m":
-                    multiplier *= 1024
+    unit = size_str[-1].lower()
+    if unit in ("k", "m"):
+        size_str = size_str[:-1]
+        multiplier = 1024
+        if unit == "m":
+            multiplier *= 1024
 
-            if not size_str:
-                return 0
+    if not size_str:
+        return 0
 
-            size = int(size_str)
-            if multiplier:
-                size *= multiplier
+    size = int(size_str)
+    if multiplier:
+        size *= multiplier
 
-            return size
+    return size
 
-        CLAMD_STREAMMAXLENGTH = _size_to_int(CLAMD_CONF["StreamMaxLength"])
-        CLAMD_MAXFILESIZE = _size_to_int(CLAMD_CONF["MaxFileSize"])
-        del conf_path, conf_lines, _size_to_int
+
+def _update_clamd_conf():
+    """Parse clamd.conf file from ENV[CLAMD_CONF_PATH] or use
+    defaults if not found."""
+    if not found_clamd:
+        return
+    conf_path = Path(os.environ.get("CLAMD_CONF_PATH") or "/etc/clamav/clamd.conf")
+    if not conf_path.exists():
+        return
+    conf_lines = [line.strip() for line in conf_path.open("rt").readlines()]
+    parsed_config = dict(
+        line.split(" ", 1) for line in conf_lines if not line.startswith("#")
+    )
+    for key in ("StreamMaxLength", "MaxFileSize"):
+        if key not in parsed_config:
+            continue
+        parsed_config[key] = _size_to_int(parsed_config[key])
+    CLAMD_CONF.update(parsed_config)
 
 
 class AntiVirusService(Service):
@@ -72,16 +83,22 @@ class AntiVirusService(Service):
         If `file_or_stream` is a Blob, scan result is stored in
         Blob.meta['antivirus'].
         """
-        if not clamd:
+        if not found_clamd:
             return None
 
-        res = self._scan(file_or_stream)
+        if not CLAMD_CONF:
+            _update_clamd_conf()
+
+        config = CLAMD_CONF_DEFAULT | CLAMD_CONF
+
+        res = self._scan(file_or_stream, config)
         if isinstance(file_or_stream, Blob):
             file_or_stream.meta["antivirus"] = res
         return res
 
-    def _scan(self, file_or_stream):
+    def _scan(self, file_or_stream, config):
         content = file_or_stream
+
         if isinstance(file_or_stream, Blob):
             # py3 compat: bytes == py2 str(). Pathlib uses os.fsencode()
             file_or_stream = bytes(file_or_stream.file)
@@ -92,24 +109,23 @@ class AntiVirusService(Service):
             content = open(file_or_stream, "rb")
 
         if content.seekable():
+            max_size = config["StreamMaxLength"]
             pos = content.tell()
             content.seek(0, io.SEEK_END)
             size = content.tell()
             content.seek(pos)
 
-            if size > CLAMD_STREAMMAXLENGTH:
+            if size > max_size:
                 logger.error(
-                    "Content size exceed antivirus size limit, size=%d, limit=%d (%s)",
-                    size,
-                    CLAMD_STREAMMAXLENGTH,
-                    CLAMD_CONF["StreamMaxLength"].encode("utf-8"),
+                    f"Content size exceed antivirus size limit, size={size}, limit={max_size}",
                     extra={"stack": True},
                 )
                 return None
 
         # use stream scan. When using scan by filename, clamd runnnig user must have
         # access to file, which we cannot guarantee
-        scan = clamd.instream
+        scan_socket = ClamdUnixSocket(config["LocalSocket"], timeout=60)
+        scan = scan_socket.instream
         try:
             res = scan(content)
         except ClamdError as e:
