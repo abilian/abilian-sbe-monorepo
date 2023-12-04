3b5135915e2d331a7fb4e509a6b214a9256394b7
Jerome Dumonteil <jd@abilian.com>
2023-12-04 15:22:09 +0100

    add Flask-Melodramatiq

--- src/abilian/sbe/apps/documents/drama_tasks.py
+++ src/abilian/sbe/apps/documents/drama_tasks.py
@@ -3,6 +3,8 @@ from collections.abc import Iterator
 from contextlib import contextmanager
 from typing import TYPE_CHECKING
 
+from sqlalchemy.sql.functions import mode
+
 import dramatiq
 from loguru import logger
 from sqlalchemy.orm import Session
@@ -11,6 +13,8 @@ from abilian.core.extensions import db
 from abilian.logutils.configure import connect_logger
 from abilian.services import converter, get_service
 from abilian.services.conversion import ConversionError, HandlerNotFound
+
+from pathlib import Path
 
 if TYPE_CHECKING:
     from .models import Document
@@ -27,8 +31,10 @@ def log_document_id(document_id: int) -> None:
     connect_logger(logger)
 
     msg = f"drama log doc id : {document_id}"
-    print(msg)
-    raise ValueError(msg)
+
+    path = Path("/var/tmp/test.txt")
+    path.write_text(msg)
+
     logger.info(msg)
 
 
