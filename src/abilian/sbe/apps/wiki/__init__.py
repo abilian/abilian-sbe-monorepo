94f2a43bc582b8b8c8294cad2089c0b9693b6975
Jerome Dumonteil <jd@abilian.com>
2024-02-26 14:34:16 +0100

    tests: fix warning on wiki.__init__ multiple wiki.record_once(register_actions)

--- src/abilian/sbe/apps/wiki/__init__.py
+++ src/abilian/sbe/apps/wiki/__init__.py
@@ -7,5 +7,9 @@ def register_plugin(app: Application) -> None:
     from .actions import register_actions
     from .views import wiki
 
-    wiki.record_once(register_actions)
+    # could also test on private attribute _got_registered_once
+    if not any(
+        fct for fct in wiki.deferred_functions if fct.__name__ == "register_actions"
+    ):
+        wiki.record_once(register_actions)
     app.register_blueprint(wiki)
