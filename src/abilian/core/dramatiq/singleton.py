2e2b2533345cc6eb5a4c7c61489f3a4abc657022
Jerome Dumonteil <jd@abilian.com>
2024-03-05 17:20:59 +0100

    fix: remove prometheus from dramatiq middleware

--- src/abilian/core/dramatiq/singleton.py
+++ src/abilian/core/dramatiq/singleton.py
@@ -1,6 +1,12 @@
 """Singleton for Dramatiq task manager, to permit lazy declaration of actors.
 """
+from __future__ import annotations
 
 from flask_dramatiq import Dramatiq
 
 dramatiq = Dramatiq()
+
+# Remove prmeteus from middleware list
+# See:
+# https://groups.io/g/dramatiq-users/topic/disabling_prometheus/80745532?p=
+dramatiq.middleware = dramatiq.middleware[1:]
