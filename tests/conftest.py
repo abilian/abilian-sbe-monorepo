7d4f1b0fd60b50ad6b65ec71af765da12e4420f0
Stefane Fermigier <sf@abilian.com>
2024-03-23 15:14:08 +0100

    refact: cleanup imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,15 @@
 """Configuration and injectable fixtures for Pytest.
 """
 
-import typing
 from collections.abc import Iterator
 from typing import Any
 
 from flask import Flask
-from flask.ctx import AppContext, RequestContext
-from flask.testing import FlaskClient
+from flask.ctx import AppContext
 from pytest import fixture
-from sqlalchemy.orm import Session
 
-from abilian.core.models.subjects import User
 from abilian.core.sqlalchemy import SQLAlchemy
 from abilian.sbe.app import create_app
-from abilian.sbe.apps.communities.models import READER, Community
 
 
 class TestConfig:
