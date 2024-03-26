10807cb00a9592e3658d81a5b721079546fc92e2
Jerome Dumonteil <jd@abilian.com>
2024-03-26 16:29:14 +0100

    upgrade ckeditor to v4.22.1

--- src/abilian/web/resources/ckeditor/config.js
+++ src/abilian/web/resources/ckeditor/config.js
@@ -1,69 +1,38 @@
 /**
- * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
 CKEDITOR.editorConfig = function( config ) {
-    'use strict';
 	// Define changes to default configuration here.
 	// For complete reference see:
-	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
-    var Abilian = require('Abilian');
-    config.defaultLanguage = Abilian.locale;
+	// https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
 
-    // http://docs.cksource.com/ckeditor_api/symbols/CKEDITOR.config.html#.entities
-    // disable accent encoding
-    config.entities = false;
-
-    config.skin = 'moono';
-
-    config.extraPlugins = ['autolink',
-                           'bootstrapVisibility'];
-    config.extraPlugins = config.extraPlugins.join(',');
-
-    config.toolbar = [
-		{ name: 'styles', items: [ 'Format' ] },
-		{ name: 'basicstyles',
-          items: [ 'Bold', 'Italic', 'Underline', '-',
-                   'RemoveFormat' ] },
-		{ name: 'paragraph',
-          items: [ 'NumberedList', 'BulletedList', '-',
-                   'Blockquote', '-',
-                   'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
-		{ name: 'links', items: [ 'Link', 'Unlink'] },
-		{ name: 'insert', items: [ 'Image'] },
-		{ name: 'clipboard', items: [ 'Paste', 'PasteText', 'PasteFromWord', '-',
-                                      'Undo', 'Redo' ] }
+	// The toolbar groups arrangement, optimized for two toolbar rows.
+	config.toolbarGroups = [
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		{ name: 'links' },
+		{ name: 'insert' },
+		{ name: 'forms' },
+		{ name: 'tools' },
+		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'others' },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+		{ name: 'styles' },
+		{ name: 'colors' },
+		{ name: 'about' }
 	];
+
+	// Remove some buttons provided by the standard plugins, which are
+	// not needed in the Standard(s) toolbar.
+	config.removeButtons = 'Underline,Subscript,Superscript';
+
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
-
-    config.allowedContent = {
-    'a': { attributes: ['!href', 'title'] },
-    'abbr': { attributes: ['title'] },
-    'acronym': { attributes: ['title'] },
-    'b': {},
-    'blockquote': { attributes: ['title'] },
-    'br': {},
-    'code': { attributes: ['title'] },
-    'em': {},
-    'i': {},
-    'li': {},
-    'ol': { attributes: ['title'] },
-    'strong': {},
-    'ul': { attributes: ['title'] },
-    'h1': {},
-    'h2': {},
-    'h3': {},
-    'h4': {},
-    'h5': {},
-    'h6': {},
-    'p': { attributes: ['style'], styles: ['text-align'] },
-    'u': {},
-    'img': { attributes: ['!src', 'alt', 'title'] }
-    };
-
 };
