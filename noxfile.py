359b7fcde5f5f6ea9ed3a9af13823fd4e5f72ed0
Stefane Fermigier <sf@abilian.com>
2024-10-29 19:27:31 +0100

    chore: migrate to uv.

--- noxfile.py
+++ noxfile.py
@@ -1,21 +1,20 @@
 import nox
 
-PYTHON_VERSIONS = ["3.10", "3.11", "3.12"]
+PYTHON_VERSIONS = ["3.10", "3.11", "3.12", "3.13"]
 
 nox.options.reuse_existing_virtualenvs = True
 
 
 @nox.session
 def lint(session: nox.Session) -> None:
-    session.install("-e", ".")
-    session.install("ruff", "flake8", "pyanalyze", "deptry", "mypy", "black", "isort")
-    session.run("pip", "check")
+    session.install("uv")
+    session.run("uv", "sync", "--inexact")
+    session.run("uv", "pip", "check")
     session.run("make", "lint", external=True)
 
 
 @nox.session(python=PYTHON_VERSIONS)
 def pytest(session: nox.Session) -> None:
-    session.install("-e", ".")
-    session.install("html5lib", "pytest", "pytest-flask", "hyperlink")
-    session.run("pip", "check")
+    session.install("uv")
+    session.run("uv", "sync", "--inexact")
     session.run("pytest", "--tb=short")
