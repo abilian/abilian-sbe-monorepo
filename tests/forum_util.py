6551b307435c48434df54a85dc7df624a916d1f5
Stefane Fermigier <sf@abilian.com>
2026-09-01 15:11:51 +0200

    refact: test pyramid

---
+++ tests/forum_util.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2012-2024, Abilian SAS
+
+from __future__ import annotations
+
+from email.feedparser import FeedParser
+
+from tests.util import DATA_DIR
+
+
+def get_string_from_file(filename="notification.email"):
+    """Load a test email, return as string."""
+    filepath = DATA_DIR / "forum" / filename
+    with filepath.open("rt", encoding="utf-8") as email_file:
+        email_string = email_file.read()
+    return email_string
+
+
+def get_email_message_from_file(filename="notification.email"):
+    """Load a mail and parse it into a email.message."""
+    email_string = get_string_from_file(filename)
+    parser = FeedParser()
+    parser.feed(email_string)
+    email_message = parser.close()
+    return email_message
